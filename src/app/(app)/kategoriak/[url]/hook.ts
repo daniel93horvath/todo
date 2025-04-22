198950e53078616b80b6804c67d125938a40fb5d
Horváth Dániel <daniel93.horvath@gmail.com>
2025-04-22 12:24:14 +0200

    2134

--- src/app/(app)/kategoriak/[url]/hook.ts
+++ src/app/(app)/kategoriak/[url]/hook.ts
@@ -9,29 +9,30 @@ export function useProducts() {
 	const path = decodeURIComponent(
 		`/api/v3/categories/${categoryUrl.url}/products?${searchParams.toString()}`
 	);
-	try {
-		const {
-			data: products,
-			isFetching,
-			isError,
-		} = useQuery<ProductsWithCategories>({
-			queryKey: ["products", path],
-			queryFn: async () => {
-				const response = await fetchGet<ProductsWithCategories>(path, {
-					baseUrl: "https://www.onlinepenztarca.hu",
-				});
-				if (!response.data) {
-					throw new Error("Sikertelen a termékek lekérdezése!");
-				}
-				return response.data;
-			},
-			staleTime: 5 * 60 * 1000,
-			placeholderData: (previousData) => previousData,
-		});
-		return { products, isFetching, isError };
-	} catch (error) {
-		alert("updateUrlWithoutReloadPage: " + JSON.stringify(error));
+
+	const {
+		data: products,
+		error,
+		isFetching,
+		isError,
+	} = useQuery<ProductsWithCategories>({
+		queryKey: ["products", path],
+		queryFn: async () => {
+			const response = await fetchGet<ProductsWithCategories>(path, {
+				baseUrl: "https://www.onlinepenztarca.hu",
+			});
+			if (!response.data) {
+				throw new Error("Sikertelen a termékek lekérdezése!");
+			}
+			return response.data;
+		},
+		staleTime: 5 * 60 * 1000,
+		placeholderData: (previousData) => previousData,
+	});
+	if (isError) {
+		alert(error);
 	}
+	return { products, isFetching, isError };
 }
 
 /**
@@ -39,6 +40,7 @@ export function useProducts() {
  * hogy újrarenderelné az oldalt
  */
 export function updateUrlWithoutReloadPage(url: string): void {
+	alert(decodeURIComponent(url));
 	console.log("UPDATE URL WITHOUT RELOAD PAGE");
 	if (typeof window === "undefined") return;
 
