7dbe394f5c396926a4aff07234126c138a568516
Horváth Dániel <daniel93.horvath@gmail.com>
2025-04-22 14:52:12 +0200

    teszt

--- next.config.ts
+++ next.config.ts
@@ -17,7 +17,7 @@ const nextConfig: NextConfig = {
 	async rewrites() {
 		return [
 			{
-				// Illeszkedik a kliens oldali /api/v3/... hívásokra
+				// Illeszkedik a kliens oldali /api/v3/... hívásokr
 				source: "/api/v3/:path*",
 				// Továbbítja a backend URL-re, levágva az /api/v3 részt a source-ból
 				destination: `${process.env.NEXT_PUBLIC_APP_URL_BACKEND}/:path*`,
